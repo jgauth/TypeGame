7bced2b7e795b7d5c0f7cf932c60edc91598c224
John Gauthier <jgauth@users.noreply.github.com>
2020-04-16 15:14:29 -0700

    Initial Commit

---
+++ Prototype1/Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class EnemyLife : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] words = File.ReadAllLines("Assets/sample_dict.txt");
+
+        string word = words[Random.Range(0, words.Length)];
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prototype1/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputHandler : MonoBehaviour
+{
+
+    public Text inputBuffer;
+
+    void Start()
+    {
+        inputBuffer.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // https://docs.unity3d.com/ScriptReference/Input-inputString.html
+
+ 
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (inputBuffer.text.Length != 0)
+                {
+                    inputBuffer.text = inputBuffer.text.Substring(0, inputBuffer.text.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+
+            }
+            else
+            {
+                inputBuffer.text += c;
+            }
+        }
+
+        if ((Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) && Input.GetKeyDown(KeyCode.Backspace))
+        {
+            inputBuffer.text = "";
+        }
+    }
+}
