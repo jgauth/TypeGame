90f0cff1801c667d683b5cbd10f746bbaccaf2de
John Gauthier <john.gauthier5@gmail.com>
2020-04-26 15:50:31 -0700

    Enemy text will now change color as the player types

--- Prototype1/Assets/Scripts/Spawner.cs
+++ Prototype1/Assets/Scripts/Spawner.cs
@@ -20,6 +20,34 @@ public class Spawner : MonoBehaviour
         enemyList.Add(e);
     }
 
+    void SpawnWordHighlightingTestEnemies()
+    {
+        float xPos = Random.Range(-4.0f, 4.0f);
+        float zPos = Random.Range(-4.0f, 4.0f);
+
+        GameObject g1 = (GameObject)Instantiate(enemyPrefab, new Vector3(xPos, 1, zPos), Quaternion.identity);
+        Enemy e1 = g1.GetComponent<Enemy>();
+        e1.SetWord("aaaa");
+        enemyList.Add(e1);
+
+        xPos = Random.Range(-4.0f, 4.0f);
+        zPos = Random.Range(-4.0f, 4.0f);
+
+        GameObject g2 = (GameObject)Instantiate(enemyPrefab, new Vector3(xPos, 1, zPos), Quaternion.identity);
+        Enemy e2 = g2.GetComponent<Enemy>();
+        e2.SetWord("aab");
+        enemyList.Add(e2);
+
+        xPos = Random.Range(-4.0f, 4.0f);
+        zPos = Random.Range(-4.0f, 4.0f);
+
+        GameObject g3 = (GameObject)Instantiate(enemyPrefab, new Vector3(xPos, 1, zPos), Quaternion.identity);
+        Enemy e3 = g3.GetComponent<Enemy>();
+        e3.SetWord("ac");
+        enemyList.Add(e3);
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +55,7 @@ public class Spawner : MonoBehaviour
         {
             SpawnEnemyRandomLocation();
         }
+
+        //SpawnWordHighlightingTestEnemies();
     }
 }
