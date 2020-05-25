a3988413bf8fbbd5e15d683405b1aa2b5b13a8c5
Miguel <39255541+margielamadman@users.noreply.github.com>
2020-05-25 14:44:57 -0700

    Update wordlists

--- TypeCaster/Assets/Scripts/WordGenerator.cs
+++ TypeCaster/Assets/Scripts/WordGenerator.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class WordGenerator : MonoBehaviour {
+public class WordGenerator : MonoBehaviour
+{
 
     public static string[] words;
 
-    void Awake() {
-        words = Resources.Load<TextAsset>("sample_dict").text.Split('\n');
+    void Awake()
+    {
+        words = Resources.Load<TextAsset>("easy").text.Split('\n');
     }
 
-    public static string GetRandomWord() {
+    public static string GetRandomWord()
+    {
         return words[Random.Range(0, words.Length)].Trim();
     }
 }
