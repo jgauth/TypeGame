715784e592e95021b7cee0d7dec7c7bcca9f2f58
Miguel <39255541+margielamadman@users.noreply.github.com>
2020-05-27 09:40:38 -0700

    Add victory and defeat screens

---
+++ TypeCaster/Assets/Scripts/DefeatMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DefeatMenu : MonoBehaviour {
+
+    public GameObject defeatScreen;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            // Temporary
+            SceneManager.LoadScene("Menu");
+        }
+        if (Input.GetKeyDown(KeyCode.M)) {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
---
+++ TypeCaster/Assets/Scripts/VictoryMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenu : MonoBehaviour {
+    public GameObject victoryScreen;
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            // Temporary
+            SceneManager.LoadScene("Menu");
+        }
+        if (Input.GetKeyDown(KeyCode.M)) {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+}
